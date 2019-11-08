cdc0a51e4eba7bc625b020c4d8f0fd83fbfc0950
amingccc <amingccc@gmail.com>
2019-11-08 19:45:01 +0800

    eureka集群配置

---
+++ euraka-7003/src/main/java/com/company/project/EurekaServer7003_App.java
@@ -0,0 +1,19 @@
+package com.company.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Chen
+ * @created 2019-11-08-12:46.
+ * @EnableEurekaServer //EurekaServer服务器端启动类,接受其它微服务注册进来
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7003_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7003_App.class, args);
+    }
+
+}
