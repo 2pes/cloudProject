3ca26e60e93d655c80161cabdae40a575478b7a9
amingccc <amingccc@gmail.com>
2019-11-08 19:03:19 +0800

    eureka注册中心项目及注册dept提供者

--- api/src/main/java/com/company/project/sys/model/Dept.java
+++ api/src/main/java/com/company/project/sys/model/Dept.java
@@ -16,9 +16,19 @@ import java.io.Serializable;
 @Data
 @Accessors(chain = true)
 public class Dept implements Serializable {
-    private Long dept_no; // 主键
-    private String dept_name; // 部门名称
-    private String db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+    /**
+     * 主键
+     */
+    private Long dept_no;
+    /**
+     * 部门名称
+     */
+    private String dept_name;
+
+    /**
+     * 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+     */
+    private String db_source;
 
     public Dept(String dept_name) {
         super();
--- provider-dept-8001/src/main/java/com/company/project/DpetProvider8001_App.java
+++ provider-dept-8001/src/main/java/com/company/project/DpetProvider8001_App.java
@@ -2,12 +2,14 @@ package com.company.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Chen
  * @created 2019-11-07-23:09.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DpetProvider8001_App {
 
     public static void main(String[] args) {
