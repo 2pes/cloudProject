3f1b8d18f8265ecee610194eb376421b9634ab67
amingccc <amingccc@gmail.com>
2019-11-10 17:45:08 +0800

    城市数据服务

---
+++ cloud-weather/cloud-weather-microservice/city-server/src/main/java/com/company/project/city/model/City.java
@@ -0,0 +1,29 @@
+package com.company.project.city.model;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Data
+@ToString
+@Accessors(chain = true)
+@XmlRootElement(name = "d")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class City {
+    @XmlAttribute(name = "d1")
+    private String cityId;
+
+    @XmlAttribute(name = "d2")
+    private String cityName;
+
+    @XmlAttribute(name = "d3")
+    private String cityCode;
+
+    @XmlAttribute(name = "d4")
+    private String province;
+}
