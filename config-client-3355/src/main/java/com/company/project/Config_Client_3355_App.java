f2e56f5a12fc1a08785018f92a0f57c26b7d88c8
amingccc <amingccc@gmail.com>
2019-11-10 11:25:24 +0800

    config客户端工程

---
+++ config-client-3355/src/main/java/com/company/project/Config_Client_3355_App.java
@@ -0,0 +1,15 @@
+package com.company.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chen
+ * @created 2019-11-08-0:10.
+ */
+@SpringBootApplication
+public class Config_Client_3355_App {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Client_3355_App.class, args);
+    }
+}
---
+++ config-client-3355/src/main/java/com/company/project/rest/ConfigClientRest.java
@@ -0,0 +1,31 @@
+package com.company.project.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Chen
+ * @created 2019-11-10-10:36.
+ */
+@RestController
+public class ConfigClientRest
+{
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaServers;
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/config")
+    public String getConfig()
+    {
+        String str = "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
+        System.out.println("******str: " + str);
+        return "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
+    }
+}
