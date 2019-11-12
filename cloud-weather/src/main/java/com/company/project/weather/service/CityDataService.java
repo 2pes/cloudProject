a40a195c062f5ac34c7ceb2bf61977026ef030eb
amingccc <amingccc@gmail.com>
2019-11-12 22:41:40 +0800

    冲突修改

--- cloud-weather/src/main/java/com/company/project/weather/service/CityDataService.java
+++
@@ -1,9 +0,0 @@
-package com.weather.spring.cloud.microweatherbasic.service;
-
-import com.weather.spring.cloud.microweatherbasic.vo.City;
-
-import java.util.List;
-
-public interface CityDataService {
-    public List<City> listCity() throws  Exception;
-}
--- cloud-weather/src/main/java/com/company/project/weather/service/WeatherReportService.java
+++
@@ -1,7 +0,0 @@
-package com.weather.spring.cloud.microweatherbasic.service;
-
-import com.weather.spring.cloud.microweatherbasic.vo.Weather;
-
-public interface WeatherReportService {
-    Weather getDataByCityId(String cityId);
-}
