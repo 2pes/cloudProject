04d9bed4c498334389bb27e4ae16969599a41f2d
amingccc <amingccc@gmail.com>
2019-11-12 21:10:54 +0800

    zuul测试

--- cloud-weather/weather-microservice/eureka-client-zuul/src/main/java/com/company/project/EurekaClientZuulApplication.java
+++ cloud-weather/weather-microservice/eureka-client-zuul/src/main/java/com/company/project/EurekaClientZuulApplication.java
@@ -3,13 +3,16 @@ package com.company.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EurekaClientApplication {
+@EnableZuulProxy
+public class EurekaClientZuulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
+        SpringApplication.run(EurekaClientZuulApplication.class, args);
     }
 
 }
